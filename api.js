1c2419b20189f948d1cf560fc0350d0d041f49b0
alumno <alumno@Instituto.local>
2025-10-01 08:22:12 -0300

    c

---
+++ api.js
@@ -0,0 +1,22 @@
+//Enlace de la api a consumir
+let contenedor = document.querySelector('.contenedor')
+fetch('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
+    .then(respuesta => respuesta.json())
+    .then(productos => {
+        for (let index = 0; index < 5; index++) {
+            console.log(`Producto en stock: ${productos[index].name}`)
+            contenedor.innerHTML += `<p>Producto en stock: ${productos[index].name}</p>
+        `
+        }
+    
+    })
+
+    fetch('https://d3t32hsnjxo7q6.cloudfront.net/i/991799d3e70b8856686979f8ff6dcfe0_ra,w158,h184_pa,w158,h184.png')
+    .then(res => res.json())
+    .then(imagenes =>{
+        for (let index = 0; index < imagenes.length; index++) {
+            let img = imagenes[i];
+            contenedor.innerHTML
+            
+        }
+    })
