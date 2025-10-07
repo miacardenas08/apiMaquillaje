060f6a8e54881d142d4f82139278760f4b653b5f
miacardenas08 <136343840+miacardenas08@users.noreply.github.com>
2025-10-07 10:38:37 -0300

    Actualizar api.js

--- api.js
+++ api.js
@@ -12,14 +12,6 @@ fetch('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
     })
 
     fetch('https://d3t32hsnjxo7q6.cloudfront.net/i/991799d3e70b8856686979f8ff6dcfe0_ra,w158,h184_pa,w158,h184.png')
-    .then(res => res.json())
-    .then(imagenes =>{
-        for (let index = 0; index < imagenes.length; index++) {
-            let img = imagenes[i];
-            contenedor.innerHTML
-            
-        }
-    })
 
 boton.onclick = function () {
 .then(productos => {
